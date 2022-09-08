25637f0ab7436ce7f616dc3af1235e0e38c0a39f
Genki <252720906@qq.com>
2022-09-08 11:51:21 +0800

    feat: implemented detectCycle

--- src/linked-list/leetcode/detectCycle.ts
+++ src/linked-list/leetcode/detectCycle.ts
@@ -1,8 +1,9 @@
 import { LinkedNode } from "../LinkedList";
 
+// https://leetcode.cn/problems/linked-list-cycle-lcci/
 // 快慢指针法的 数学公式推导基础
 // a + (nb + nc + c) = 2(a + b)  =======> a = (n - 1)(b + c) + c
-export const hasCycle = (head: LinkedNode) => {
+export const detectCycle = (head: LinkedNode) => {
   let slow = head;
   let fast = head;
 
@@ -10,10 +11,15 @@ export const hasCycle = (head: LinkedNode) => {
     slow = slow.next;
     fast = fast.next.next;
     if (slow === fast) {
-      return true;
+      let pointer = head;
+      while (pointer !== slow) {
+        pointer = pointer.next;
+        slow = slow.next;
+      }
+
+      return pointer;
     }
   }
 
-  return false;
+  return null;
 };
-
